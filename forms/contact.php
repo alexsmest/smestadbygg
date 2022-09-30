52a037d508223f53dd452934c3c7ca68c7074991
alig2040 <113180603+alig2040@users.noreply.github.com>
2022-09-30 10:51:42 +0200

    test

--- forms/contact.php
+++ forms/contact.php
@@ -19,7 +19,7 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['navn'];
+  $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
